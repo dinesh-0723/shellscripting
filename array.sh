032ebea908444b3d5e08cec09326a1bb4d5cd870
dinesh-0723 <dineshyadav0723@gmail.com>
2022-03-17 11:33:42 +0530

    accessing array values

--- array.sh
+++ array.sh
@@ -1,6 +1,6 @@
-a=(10,20,30,40)
-echo "the first value is:"${a[0]}
-echo "the second value is"${a[1]}
-echo "the third value is"${a[2]}
-echo "the fourth value is"${a[3]}
+a=(10 20 30 40)
+echo the first value is=${a[0]}
+echo the second value is=${a[1]}
+echo the third value is=${a[2]}
+echo the fourth value is=${a[3]}
 echo training= ${training}
