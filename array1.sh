c8a91b2ca0f66d01f6fd82443b445f058711f369
dinesh-0723 <dineshyadav0723@gmail.com>
2022-03-17 11:55:17 +0530

    accessing array values

---
+++ array1.sh
@@ -0,0 +1,5 @@
+a=(10 20 30 40)
+echo first element in array is ${a[0]}
+echo second element in array is ${a[1]}
+echo the third element in array is ${a[2]}
+echo the fourth element in array is ${a[3]}
