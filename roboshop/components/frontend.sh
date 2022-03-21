990f142690624870630e3b5645372138b4045826
dinesh-0723 <dineshyadav0723@gmail.com>
2022-03-21 12:38:11 +0530

    iam displaying messages with color codes

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
+echo -e "\e[36m Installing nginx \e[0m"
 yum install nginx -y
+echo -e "\e[36m downloading nginx content \e[0m"
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 rm -rf /usr/share/nginx/html/*
@@ -10,5 +11,6 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
+echo -e "\e[starting nginx \e[0m"
 systemctl restart nginx
 systemctl enable nginx
