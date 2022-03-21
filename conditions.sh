8489ac2a25b6522e2c94f1f17ecef3cca0adbe26
dinesh-0723 <dineshyadav0723@gmail.com>
2022-03-22 03:55:03 +0530

    i created simple if statement

--- conditions.sh
+++ conditions.sh
@@ -2,3 +2,11 @@ if [ 1 -eq 1 ]
 then
 echo "hello world"
 fi
+a=abc
+if [ "$a"=="abc" ];then
+echo "both are equal"
+if [ -z "$a" ];then
+  echo "variable is not empty"
+  if [ -z "$b" ];then
+  echo "variable is empty"
+  fi
