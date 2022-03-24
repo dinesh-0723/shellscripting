e3d5fcccb6bebd7d99a5d20a8fe38a0c72d34619
dinesh-0723 <dineshyadav0723@gmail.com>
2022-03-24 18:54:07 +0530

    mongodb setup

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
-statcheck() {
-  if [ $1 -eq 0 ];then
-    echo -e "\e[32m success \e[0m"
-    else
-      echo -e "\e[31m failure \e[0m"
-     exit 2
-  fi
-}
-print() {
-  echo -e "\n.......$1.............." &>>$LOG_FILE
-  echo -e "\e[36m $1 \e[0m"
-}
-
-USER_ID=$(id -u)
-if [ "$USER_ID" -ne 0 ];then
-  echo "you should run your script as root user"
-  exit 1
-fi
-LOG_FILE=/tmp/roboshop.log
-rm -rf $LOG_FILE
-
+source components/common.sh
 print "Setup MongoDB"
 curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>LOG_FILE
 statcheck $?
