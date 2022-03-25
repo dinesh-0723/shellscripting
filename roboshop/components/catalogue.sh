8bf8461166c514a14087947c82e3ba38dbebaa00
dinesh-0723 <dineshyadav0723@gmail.com>
2022-03-25 05:39:16 +0530

    catalogue setup

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,2 +1,34 @@
 #!/bin/bash
-echo frontendcomponent
+source components/common.sh
+
+print "configure repos"
+curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash - &>>${LOG_FILE}
+statcheck $?
+
+print "install nodejs"
+yum install nodejs gcc-c++ -y &>>${LOG_FILE}
+statcheck $?
+
+print "add application user"
+id ${APP_USER} &>>${LOG_FILE}
+if [ $? -ne 0]; then
+  useradd ${APP_USER} &>>${LOG_FILE}
+fi
+statcheck $?
+
+print "download app component"
+curl -f -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
+statcheck $?
+
+print "cleanup old content"
+rm -rf /home/${APP_USER}/catalogue &>>${LOG_FILE}
+
+print "extract app content"
+cd /home/${APP_USER} &>>LOG_FILE && unzip -o /tmp/catalogue.zip &>>${LOG_FILE} && mv catalogue-main catalogue &>>${LOG_FILE}
+statcheck $?
+
+print "install app dependencies"
+cd /home/${APP_USER}/catalogue &>>${LOG_FILE} && npm install &${LOG_FILE}
+statcheck $?
+
+
