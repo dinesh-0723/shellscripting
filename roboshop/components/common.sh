6d4fa9472d08e6737aca746c73d10e8a2b672592
dinesh-0723 <dineshyadav0723@gmail.com>
2022-03-29 11:31:30 +0530

    changes

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -58,7 +58,7 @@ NODEJS() {
   statcheck $?
 
   print "setup systemd file"
-  sed -i -e 's/MONGO_DNSNAME/monodsh.roboshop.internal/' -e 's/REDIS_ENDPOINT/newredis.roboshop.internal/' -e 's/MONGO_ENDPOINT/monodsh.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catsh.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>${LOG_FILE} && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>${LOG_FILE}
+ sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>${LOG_FILE} && mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>${LOG_FILE}
   statcheck $?
 
   print "start ${COMPONENT} service"
