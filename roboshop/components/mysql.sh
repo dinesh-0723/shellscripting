e4d1503285cdb7447200d231f23f20b7b04f8316
dinesh-0723 <dineshyadav0723@gmail.com>
2022-03-30 12:07:50 +0530

    added mysql component

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -11,7 +11,17 @@ yum install mysql-community-server -y &>>${LOG_FILE}
 statcheck $?
 
 print "start mysql"
-
 systemctl enable mysqld &>>${LOG_FILE} && systemctl start mysqld &>>${LOG_FILE}
 statcheck $?
+
+echo 'show databases' | mysql -uroot -pRoboShop@1 &>>${LOG_FILE}
+if [ $? -ne 0 ]; then
+  print "change default root password"
+echo "SET PASSWORD FOR 'root' '@' 'localhost' = PASSWORD('RoboShop@1');" >/tmp/rootpass.sql
+DEFAULT_ROOT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+mysql --connect-expired-password -uroot -p"${DEFAULT_ROOT_PASSWORD}" </tmp/rootpass.sql &>>${LOG_FILE}
+statcheck $?
+fi
+
+
 
