01ade8735d3718488cccd89d11af2fb0da384cc7
dinesh-0723 <dineshyadav0723@gmail.com>
2022-04-03 19:16:22 +0530

    instance creation

--- createec2.sh
+++ createec2.sh
@@ -5,6 +5,7 @@ if [ -z "$1" ]; then
   exit 1
   fi
   COMPONENT=$1
+  ZONE_ID="Z0978146KR3LELI7PGRT"
 
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=allow-all-from-public | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g'
@@ -18,3 +19,5 @@ PRIVATE_IP=$(aws ec2 run-instances \
     --security-group-ids=${SG_ID} \
      | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
+sed -e "s/IPADDRESS/${PRIVATE_IP}/" -e "s/COMPONENT/${COMPONENT}/" route53.json >/tmp/record.json
+aws route53 change-resource-record-sets --hosted-zone-id ${ZONE_ID} --change-batch file:///tmp/record.json | jq
