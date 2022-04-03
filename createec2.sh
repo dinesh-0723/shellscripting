99b780cb4e5b6d09921ab8a0be313b5013560a4d
dinesh-0723 <dineshyadav0723@gmail.com>
2022-04-03 10:55:52 +0530

    instance creation

--- createec2.sh
+++ createec2.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-AMI ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId')
-echo $AMI ID
+AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId')
+echo $AMI_ID
