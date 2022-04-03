c0b88cf5a4848b014bbac0e102ace3673a177129
dinesh-0723 <dineshyadav0723@gmail.com>
2022-04-03 15:26:54 +0530

    instance creation

--- createec2.sh
+++ createec2.sh
@@ -10,11 +10,11 @@ AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Pra
 SG_ID=$(aws ec2 describe-security-groups --filters Name=group-name,Values=allow-all-from-public | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g'
 )
 echo $AMI_ID
-aws ec2 run-instances \
+PRIVATE_IP=$(aws ec2 run-instances \
 --image-id ${AMI_ID} \
  --instance-type t3.small \
   --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=${COMPONENT}}]" \
    --instance-market-options "MarketType=spot,SpotOptions={SpotInstanceType=persistent,InstanceInterruptionBehavior=stop}" \
     --security-group-ids=${SG_ID} \
-     | jq
+     | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g')
 
