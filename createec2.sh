8888e7e1b651f044331dc1ec47edd102529ff144
dinesh-0723 <dineshyadav0723@gmail.com>
2022-04-03 14:07:09 +0530

    instance creation

--- createec2.sh
+++ createec2.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+
+if [ -z "$1" 0 ]; then
+  exit 1
+  fi
+  COMPONENT=$1
+echo "input machine name is needed"
 AMI_ID=$(aws ec2 describe-images --filters "Name=name,Values=Centos-7-DevOps-Practice" | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo $AMI_ID
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.small
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.small --tag-specifications "ResourceType=instance,Tags=[{Key=name,Value=${COMPONENT}]"
 
 
